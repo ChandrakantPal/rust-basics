52094487fd1eefc0cb5d6d98078987a303d95b80
ChandrakantPal <sanjay161097@gmail.com>
2022-04-14 19:11:41 +0530

    operator

--- rust-basics/calculator/src/main.rs
+++ rust-basics/calculator/src/main.rs
@@ -3,5 +3,7 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
     let first = args.nth(1).unwrap();
+    let operator = args.nth(2).unwrap();
+
     println!("{:?}", first);
 }
