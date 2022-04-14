f0f6a065fc9c3784767450faef1333a22151d567
ChandrakantPal <sanjay161097@gmail.com>
2022-04-14 19:10:18 +0530

    printing arg 1

--- rust-basics/calculator/src/main.rs
+++ rust-basics/calculator/src/main.rs
@@ -2,6 +2,6 @@ use std::env::{args, Args};
 
 fn main() {
     let mut args: Args = args();
-    let first = args.nth(0).unwrap();
+    let first = args.nth(1).unwrap();
     println!("{:?}", first);
 }
