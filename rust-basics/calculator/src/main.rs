c0ddcff19660291b3c203609f3d74c8a1e1f3cd5
ChandrakantPal <sanjay161097@gmail.com>
2022-04-15 12:09:47 +0530

    acessing 0th element

--- rust-basics/calculator/src/main.rs
+++ rust-basics/calculator/src/main.rs
@@ -3,8 +3,8 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
     let first = args.nth(1).unwrap();
-    let operator = args.nth(2).unwrap();
-    let second = args.nth(3).unwrap();
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
 
     println!("{:?} {} {}", first, operator, second);
 }
