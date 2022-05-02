f37c3ded440c2eae2913fc7e8f4ff97d5aa668ca
ChandrakantPal <sanjay161097@gmail.com>
2022-05-02 23:31:53 +0530

    return keyword removed

--- rust-basics/calculator/src/main.rs
+++ rust-basics/calculator/src/main.rs
@@ -14,13 +14,13 @@ fn main() {
 
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     if operator == '+' {
-        return first_number + second_number;
+        first_number + second_number
     } else if operator == '-' {
-        return first_number - second_number;
+        first_number - second_number
     } else if operator == '/' {
-        return first_number / second_number;
+        first_number / second_number
     } else if operator == '*' {
-        return first_number * second_number;
+        first_number * second_number
     } else {
         0.0
     }
