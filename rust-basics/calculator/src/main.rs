58f613041e266abd79200854bf4e409c47eaca9e
ChandrakantPal <sanjay161097@gmail.com>
2022-05-02 23:33:28 +0530

    string to char

--- rust-basics/calculator/src/main.rs
+++ rust-basics/calculator/src/main.rs
@@ -3,7 +3,7 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
     let first = args.nth(1).unwrap();
-    let operator = args.nth(0).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
 
     let first_number = first.parse::<f32>().unwrap();
