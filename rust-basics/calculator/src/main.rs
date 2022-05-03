7b2fd6985a83b96c7eb81d7e8b387eff633bd052
ChandrakantPal <sanjay161097@gmail.com>
2022-05-03 12:37:45 +0530

    sub

--- rust-basics/calculator/src/main.rs
+++ rust-basics/calculator/src/main.rs
@@ -19,6 +19,7 @@ fn main() {
 fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     match operator {
         '+' => first_number + second_number,
+        '-' => first_number - second_number,
     }
 }
 
