afc94bce3f8a5c946ba12af36339db3a521ff378
ChandrakantPal <sanjay161097@gmail.com>
2022-05-03 12:38:13 +0530

    divide

--- rust-basics/calculator/src/main.rs
+++ rust-basics/calculator/src/main.rs
@@ -20,6 +20,7 @@ fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
     match operator {
         '+' => first_number + second_number,
         '-' => first_number - second_number,
+        '/' => first_number / second_number,
     }
 }
 
