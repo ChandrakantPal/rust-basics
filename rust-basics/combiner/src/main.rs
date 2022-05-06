ffd0b055f2503125a201257f6695b553390e3d1e
ChandrakantPal <sanjay161097@gmail.com>
2022-05-06 19:54:25 +0530

    Image

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -9,6 +9,6 @@ fn main() {
 fn find_image_from_path(path: String) {
     let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
     let image_format: ImageFormat = image_reader.format().unwrap();
-    let image = image_reader.decode().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
     (image, image_format)
 }
