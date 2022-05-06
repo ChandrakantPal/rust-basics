8d447f3a26c827caff255678d9a6196caab88fd2
ChandrakantPal <sanjay161097@gmail.com>
2022-05-06 19:53:35 +0530

    Reader type

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 }
 
 fn find_image_from_path(path: String) {
-    let image_reader = Reader::open(path).unwrap();
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
     let image_format = image_reader.format().unwrap();
     let image = image_reader.decode().unwrap();
     (image, image_format)
