5f3d67173893b53426116dea7876a9bac7b077a0
ChandrakantPal <sanjay161097@gmail.com>
2022-05-06 19:51:23 +0530

    image decode

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -9,4 +9,5 @@ fn main() {
 fn find_image_from_path(path: String) {
     let image_reader = Reader::open(path).unwrap();
     let image_format = image_reader.format().unwrap();
+    let image = image_reader.decode().unwrap();
 }
