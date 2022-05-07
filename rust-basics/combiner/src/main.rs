a98d8312c3fff47a02f882f5f78e0d03518bdcf4
ChandrakantPal <sanjay161097@gmail.com>
2022-05-07 22:41:40 +0530

    diffrentImageFormats

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -3,14 +3,16 @@ use args::Args;
 use image::{io::Reader, DynamicImage, ImageFormat};
 use std::{fs::File, io::BufReader};
 
-enum ImageDataErrors {}
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
 
 fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
     let (image_1, image_format_1) = find_image_from_path(args.image_1);
     let (image_2, image_format_2) = find_image_from_path(args.image_2);
     if image_format_1 != image_format_2 {
-        return Err();
+        return Err(ImageDataErrors::DifferentImageFormats);
     }
     Ok(())
 }
