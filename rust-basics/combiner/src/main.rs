aa1313d63530607a904a3595afa8b2a31497373e
ChandrakantPal <sanjay161097@gmail.com>
2022-05-09 11:59:47 +0530

    FloatingImage used

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -40,6 +40,7 @@ fn main() -> Result<(), ImageDataErrors> {
     }
 
     let (image_1, image_2) = standardise_size(image_1, image_2);
+    let output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
     Ok(())
 }
 
