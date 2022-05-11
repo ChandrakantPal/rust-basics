9ee5e0276b58f2bd26f938a10d068986a83f2e92
ChandrakantPal <sanjay161097@gmail.com>
2022-05-11 22:57:33 +0530

    combine_images function

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -67,3 +67,5 @@ fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicIma
         (image_1, image_2.resize_exact(width, height, Triangle))
     }
 }
+
+fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {}
