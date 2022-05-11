2c4a8c95ac810cb6fe45b03c889e9bcbdea8a167
ChandrakantPal <sanjay161097@gmail.com>
2022-05-11 23:22:24 +0530

    alternate_pixels used

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -71,6 +71,7 @@ fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicIma
 fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
     let vec_1 = image_1.to_rgb8().into_vec();
     let vec_2 = image_2.to_rgb8().into_vec();
+    alternate_pixels(vec_1, vec_2)
 }
 
 fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {}
