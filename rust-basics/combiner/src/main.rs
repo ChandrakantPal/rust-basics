0f283c0d1d2ded91e62a1f9b383a1e85cd2f2f6e
ChandrakantPal <sanjay161097@gmail.com>
2022-05-11 23:27:32 +0530

    while vec_1.len

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -77,4 +77,9 @@ fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
 fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
     // if vec_1.len() == 5, [0,0,0,0,0]
     let combine_data = vec![0u8; vec_1.len()];
+
+    let mut i = 0;
+    while i < vec_1.len() {
+        i += 4;
+    }
 }
