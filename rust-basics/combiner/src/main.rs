965a14c1ca4f8664dd8b5c0038c5e362010d98df
ChandrakantPal <sanjay161097@gmail.com>
2022-05-13 01:59:13 +0530

    result issue resolved

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -45,8 +45,8 @@ impl FloatingImage {
 
 fn main() -> Result<(), ImageDataErrors> {
     let args = Args::new();
-    let (image_1, image_format_1) = find_image_from_path(args.image_1);
-    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+    let (image_1, image_format_1) = find_image_from_path(args.image_1)?;
+    let (image_2, image_format_2) = find_image_from_path(args.image_2)?;
     if image_format_1 != image_format_2 {
         return Err(ImageDataErrors::DifferentImageFormats);
     }
