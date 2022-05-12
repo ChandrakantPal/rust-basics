9ed83573e8fbaf6d17cb2329f658a1bf394b5581
ChandrakantPal <sanjay161097@gmail.com>
2022-05-13 02:04:43 +0530

    UnableToSaveImage(e)

--- rust-basics/combiner/src/main.rs
+++ rust-basics/combiner/src/main.rs
@@ -14,6 +14,7 @@ enum ImageDataErrors {
     UnableToReadImageFromPath(std::io::Error),
     UnableToFormatImage(String),
     UnableToDecodeImage(ImageError),
+    UnableToSaveImage(ImageError),
 }
 
 struct FloatingImage {
